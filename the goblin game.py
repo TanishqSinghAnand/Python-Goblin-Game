50572f95d9ba24e142dcc0093e70375e339f7281
Tanishq0504 <codertanihsq@gmail.com>
2020-10-25 12:52:03 +0530

    Loaded Sprites

---
+++ the goblin game.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init()
+
+win=pygame.display.set_mode((500,480))
+
+pygame.display.set_caption("The Goblin Game")
+
+walkRight=pygame.image.load('R1.png'),pygame.image.load('R2.png'),pygame.image.load('R3.png'),pygame.image.load('R4.png'),pygame.image.load('R5.png'),pygame.image.load('R6.png'),pygame.image.load('R7.png'),pygame.image.load('R8.png'),pygame.image.load('R9.png') 
+walkLeft=pygame.image.load('L1.png'),pygame.image.load('L2.png'),pygame.image.load('L3.png'),pygame.image.load('L4.png'),pygame.image.load('L5.png'),pygame.image.load('L6.png'),pygame.image.load('L7.png'),pygame.image.load('L8.png'),pygame.image.load('L9.png')
